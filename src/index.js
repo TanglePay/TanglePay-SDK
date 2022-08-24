20722b1a916bf363ce960f0a19b41ab9ae33fbdf
daihanqiao <daihanqiao@126.com>
2022-08-24 10:37:45 +0800

    fix eth_sign

--- src/index.js
+++ src/index.js
@@ -71,6 +71,7 @@ const iotaSDK = {
         if (!iotaSDK.isTanglePay) {
             toInstall()
         }
+        method = method !== 'eth_sign' ? method : 'iota_sign'
         return new Promise((resolve, reject) => {
             window[`iota_request_${method}`] = function (res, code) {
                 if (code === 200) {
