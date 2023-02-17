9cd159bc5bb2ddc8bda4a0ad68dc441c5c135747
mengyuancmu <raslinyuan@gmail.com>
2023-02-17 09:34:27 +0800

    update plugin dependency

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -1,8 +1,8 @@
 import resolve from "@rollup/plugin-node-resolve";
 import babel from "@rollup/plugin-babel";
-import commonjs from "rollup-plugin-commonjs";
-import typescript from "rollup-plugin-typescript";
-import { terser } from 'rollup-plugin-terser'
+import commonjs from "@rollup/plugin-commonjs";
+import typescript from "@rollup/plugin-typescript";
+import terser from '@rollup/plugin-terser'
 import filesize from 'rollup-plugin-filesize'
 import pkg from "./package.json" assert { type: "json" };
 
